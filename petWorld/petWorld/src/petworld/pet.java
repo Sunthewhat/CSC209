c4efc1f87a5148a58e3528249f3a571517d11fa7
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-01-25 09:54:21 +0700

    sd

--- petWorld/petWorld/src/petworld/pet.java
+++ petWorld/petWorld/src/petworld/pet.java
@@ -15,19 +15,101 @@ public class Pet {
     Date DateOfBirth;
     String[] color;
     int ID;
-    String type;
-    String gender;
+    private String type;
+    private String gender;
+    private Vaccine[] vaccines;
+    private int vaccineCount;
 
     public Pet(String name, int ID, String gender) {
         this.name = name;
         this.ID = ID;
-        this.gender = gender;
+        this.setGender(gender);
+        vaccines = new Vaccine[100];
+        vaccineCount = 0;
+   }
+    
+    public int vaccinate(Vaccine v) {
+        vaccines[vaccineCount++] = v;
+        return vaccineCount;
+    }
+    
+    public void showAllPreventDiseases() {
+        if (vaccineCount>0) {
+            System.out.print("The pet is safe from: ");
+            for (int i = 0; i < vaccineCount; i++) {
+                System.out.print(vaccines[i].getPreventedDisease()+" ");
+            }
+            System.out.println("");
+        }
+        else System.out.println("This pet is at all risk of all diseases.");
     }
 
     public void setGender(String gender) {
-        if (gender.equalsIgnoreCase("male")||gender.equalsIgnoreCase("female")) {
+        if (gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female")) {
             this.gender = gender;
+        } else {
+            System.out.println("Animal genders are only male or female.");
+            this.gender = "Unknown";
         }
-        else System.out.println("Animal genders are only male or female.");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public String[] getColor() {
+        return color;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getVaccineCount() {
+        return vaccineCount;
+    }
+    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(Date DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public void setColor(String[] color) {
+        this.color = color;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public boolean setType(String type) {
+        String[] typeList = {"dog","cat","fish","rabbit","bird","chicken","python","raptor","worm","cricket"};
+        for (int i = 0; i < typeList.length; i++) {
+            if (type.equalsIgnoreCase(typeList[i])) {
+                this.type = type;
+                return true;
+            }
+        }
+        this.type = "Unknown";
+        return false;
+    }
+    
+    
+    
 }
