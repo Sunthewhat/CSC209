859af63c5dd59b94dd97a2898080364c5a9dec76
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-01-31 19:03:49 +0700

    changes

--- Restaurant/src/restaurant/Menu.java
+++ Restaurant/src/restaurant/Menu.java
@@ -1,6 +1,30 @@
 package restaurant;
 
+import java.util.*;
+
 public class Menu {
     Food[] foods;
-    
+    int amountOfFood;
+
+    public Menu() {
+        foods = new Food[30];
+        amountOfFood = 0;
+    }
+
+    public int addNewFood(Food newFood) {
+        foods[amountOfFood] = newFood;
+        return amountOfFood++;
+    }
+
+    public boolean removeFood(int foodID) {
+        for (int i = 0; i < amountOfFood; i++) {
+            if (foods[i].ID == foodID) {
+                for (int j = i; j < amountOfFood - 1; j++) {
+                    foods[j] = foods[j + 1];
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 }
