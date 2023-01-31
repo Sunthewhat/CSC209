d512e6d46ad6e5e5c2db524b546f003b9f9ab4f5
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-01-31 18:53:20 +0700

    modify restaurants

--- Restaurant/src/restaurant/Restaurant.java
+++ Restaurant/src/restaurant/Restaurant.java
@@ -16,6 +16,18 @@ public class Restaurant {
     public static void main(String[] args) {
         Food padthai = new Food(1, "padthai", 49, 200, (short) 4);
         System.out.println(padthai.toString());
+        padthai.setCategory("noodle");
+        padthai.addIngredient("shrimp");
+        padthai.addIngredient("noodle");
+        padthai.addIngredient("bean sprout");
+        padthai.addIngredient("egg");
+        System.out.println(padthai.ingredients.elementAt(0) + " " + padthai.ingredients.elementAt(2));
+        Food tomyum = new Food(2, "Tomyum", 249.50, 500, (short) 5);
+        tomyum.setCategory("seafood");
+        tomyum.addIngredient("shrimp");
+        tomyum.addIngredient("lemon");
+        tomyum.addIngredient("chilli pepper");
+        
     }
 
 }
